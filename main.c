95babab09bce220a31817fbe7246de239f07ed13
Mikael Shumacher <mikaelabrell50@gmail.com>
2025-07-12 13:46:47 -0300

    init

--- main.c
+++ main.c
@@ -4,12 +4,21 @@
 	Versão: 0.0.1
  	Jogar poker via linha de comando no seu terminal.
 */
-
 //Bibliotecas:
 #include<stdio.h>
 
 
+void init(){
+	printf("=======================================================\n");
+	printf("\tPoker feito em linguagem C\n");
+	printf("\tDe: Mikael Shumacher\n");
+	printf("=======================================================\n");
+
+}
+
+
+
 //Função principal: 
 int main(){
-	printf("Poker feito em linguagem C\n");
+	init();
 }
